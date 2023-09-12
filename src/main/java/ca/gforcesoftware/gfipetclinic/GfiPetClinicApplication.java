3b657d6fe947be8acfdb01ec011d6bbbe84e9044
Gavin Hashemi <Gavin.Hashemi@gforcesoftware.ca>
2023-09-12 16:00:13 -0400

    Initial project commit

---
+++ src/main/java/ca/gforcesoftware/gfipetclinic/GfiPetClinicApplication.java
@@ -0,0 +1,13 @@
+package ca.gforcesoftware.gfipetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfiPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfiPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/gforcesoftware/gfipetclinic/GfiPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gforcesoftware.gfipetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GfiPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
