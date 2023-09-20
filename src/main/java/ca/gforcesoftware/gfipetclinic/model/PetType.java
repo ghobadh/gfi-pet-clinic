0a6ffc7b9a535c02cd6d4dd781dc2cd065840ebf
Gavin Hashemi <Gavin.Hashemi@gforcesoftware.ca>
2023-09-20 10:11:26 -0400

    Initial project commit

---
+++ src/main/java/ca/gforcesoftware/gfipetclinic/model/PetType.java
@@ -0,0 +1,17 @@
+package ca.gforcesoftware.gfipetclinic.model;
+
+/**
+ * @author gavinhashemi on 2023-09-20
+ */
+public class PetType {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+}
---
+++ src/main/java/ca/gforcesoftware/gfipetclinic/model/Vet.java
@@ -0,0 +1,7 @@
+package ca.gforcesoftware.gfipetclinic.model;
+
+/**
+ * @author gavinhashemi on 2023-09-20
+ */
+public class Vet extends Person {
+}
